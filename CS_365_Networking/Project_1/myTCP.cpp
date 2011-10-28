0dbf2877caa2ea48de608ac3cb346558a28d77d1
James Anderson <jra798@mst.edu>
2011-10-27 22:57:56 -0500

    added a little to my TCP

--- CS_365_Networking/Project_1/myTCP.cpp
+++ CS_365_Networking/Project_1/myTCP.cpp
@@ -37,7 +37,8 @@ void tcp_server_init(int port_number)
 {
 	//Server
     server = true;
-    
+    net.init(port_number);
+	
     //Start threads
 	if(pthread_create(&sender, NULL, send_thread, NULL))
 	{
@@ -58,7 +59,10 @@ void tcp_client_init(char * ip_address, int port_number)
 {
 	//Client
     server = false;
-
+    strcpy(server_ip_address, ip_address, 4);
+    net.init(port_number, server_ip_address);
+    
+    
     //Start threads
 	if(pthread_create(&sender, NULL, send_thread, NULL))
 	{
@@ -91,29 +95,33 @@ int tcp_recv(const void *buffer , size_t bufferLength)
 
 void * send_thread(void *arg)
 {
+    if(server)
+    {
+        
+    }
+    else
+    {
+        //setup header for intial syn
+        client_header.tcp_hdr.source = 0;
+        client_header.tcp_hdr.dest = 0;
+        client_header.tcp_hdr.seq = CLIENT_ISN;
+        client_header.tcp_hdr.ack_seq = 0;
+        client_header.tcp_hdr.res1 = 0;
+        client_header.tcp_hdr.doff = 0;
+        client_header.tcp_hdr.fin = 0;
+        client_header.tcp_hdr.syn = 1;
+        client_header.tcp_hdr.rst = 0;
+        client_header.tcp_hdr.psh = 0;
+        client_header.tcp_hdr.ack = 0;
+        client_header.tcp_hdr.urg = 0;
+        client_header.tcp_hdr.res2 = 0;
+        client_header.tcp_hdr.window = 0;
+        client_header.tcp_hdr.check = 0;
+        client_header.tcp_hdr.urg_ptr = 0;
+        client_header.data_len = 0;
+    }
     
-    net.init(server_port_number, server);
-	//strcpy(server_ip_address, ip_address, 4);
-    
-    /*
-    //setup header for intial syn
-    client_header.tcp_hdr.source = 0;
-    client_header.tcp_hdr.dest = 0;
-    client_header.tcp_hdr.seq = CLIENT_ISN;
-    client_header.tcp_hdr.ack_seq = 0;
-    client_header.tcp_hdr.res1 = 0;
-    client_header.tcp_hdr.doff = 0;
-    client_header.tcp_hdr.fin = 0;
-    client_header.tcp_hdr.syn = 1;
-    client_header.tcp_hdr.rst = 0;
-    client_header.tcp_hdr.psh = 0;
-    client_header.tcp_hdr.ack = 0;
-    client_header.tcp_hdr.urg = 0;
-    client_header.tcp_hdr.res2 = 0;
-    client_header.tcp_hdr.window = 0;
-    client_header.tcp_hdr.check = 0;
-    client_header.tcp_hdr.urg_ptr = 0;
-    client_header.data_len = 0;
+
     
     //add header to no data
     //send 
@@ -124,32 +132,33 @@ void * send_thread(void *arg)
 
 void * recv_thread(void *arg)
 {
-    /*
-    
-    //initalize udp
-	net.init(port_number, server);
-	
-    while()
+     
+    if(server)
     {
-        //recieve
-        //strip header to client header
-        if(client_header.tcp_hdr.syn)
+        while()
         {
-            server_header = client_header;
-            server_header.tcp_hdr.ack = 1;
-            server_header.tcp_hdr.seq = CLIENT_ISN;
-            server_header.tcp_hdr.ack_seq = server_header.tcp_hdr.seq + 1;
-            
-            //add header to no data
-            //send to client
-            
-            //wait for recept
-            
-            
-            
+            //recieve
+            //strip header to client header
+            if(client_header.tcp_hdr.syn)
+            {
+                server_header = client_header;
+                server_header.tcp_hdr.ack = 1;
+                server_header.tcp_hdr.seq = CLIENT_ISN;
+                server_header.tcp_hdr.ack_seq = server_header.tcp_hdr.seq + 1;
+                
+                //add header to no data
+                //send to client
+                
+                //wait for recept
+                
+                
+                
+            }
         }
     }
-    */
+    else
+    {
+    }   
 }
 
 
