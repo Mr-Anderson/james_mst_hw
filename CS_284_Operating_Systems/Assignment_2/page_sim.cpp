833f97f3ab1cd8fcb0dedfc7bfdf3fa949f9d6c0
James Anderson <jra798@mst.edu>
2011-10-31 15:02:05 -0500

    added inputs checks

--- CS_284_Operating_Systems/Assignment_2/page_sim.cpp
+++ CS_284_Operating_Systems/Assignment_2/page_sim.cpp
@@ -16,22 +16,42 @@ int main(int argc, char **argv)
     // check to see if their are the correct number of arguments
     if (argc != 6)
     {
-        fputs(" usage: page_sim <programlist file> <programtrace file> <page size> <page replacement algorithum : lru/fifo/clock > <prepageing/demandpaging : p/d> \r \n", stderr);
+        fputs(" usage: page_sim <programlist file> <programtrace file> <page size> <page replacement algorithum : lru/fifo/clock > <prepaging/demandpaging : p/d> \r \n", stderr);
         exit(0);
     }
     
     //open program list file
 
     programlist_fp = fopen (argv[1] , "r");
-    if (programlist_fp == NULL) perror ("Error opening program trace file");
+    if (programlist_fp == NULL) perror ("Error opening program list file");
     
     programtrace_fp = fopen (argv[2] , "r");
-    if (programtrace_fp == NULL) perror ("Error opening program list file");
+    if (programtrace_fp == NULL) perror ("Error opening program trace file");
     
+    page_size = argv[3];
+    //do page cration stuff
     
-    //Open input and output file
-    in_fp=fopen (argv[3] , "r");
-    out_fp=fopen (argv[4] , "w+");
+    //get mode
+    if(argv[5] == p || argv[5] == P) mode = prepaging;
+    else if(argv[5] == d || argv[5] == D) mode = demandpaging;
+    else perror ("Cannot determine paging mode selection");
+    
+    if(argv[4] == lru || argv[4] == LRU)
+    {
+        algo == lru;
+        // do lru stuff
+    }
+    else if(argv[4] == fifo || argv[4] == FIFO) 
+    {
+        algo = fifo;
+        // do fifo stuff
+    }
+    else if(argv[4] == clock || argv[4] == CLOCK) 
+    {
+        algo = clock;
+    }
+    else perror ("Cannot determine paging algorithum selection");
+    
 
 
 }
