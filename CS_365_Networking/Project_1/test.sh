0f0a33d2c6c2a7648397db227c4d0c88cc13cc1b
James Anderson <jra798@mst.edu>
2011-11-06 16:04:45 -0600

    fixed scripts

--- CS_365_Networking/Project_1/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./client 127.0.0.1 9877 testfile.jpg recv.jpg
---
+++ CS_365_Networking/Project_1/test_srv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Author: James Anderson 
+# Class: CS284
+# Assignment: 2 run script
+
+make
+./server 9877
+
+#valgrind --leak-check=yes 
